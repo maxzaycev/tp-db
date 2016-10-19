a119efc5d05424a843c48daeb306b8a173a1e640
Maksim Zaytsev <zaytsev@mail.com>
2016-10-19 15:30:11 +0300

    Sequrity features

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : 'localhost',
   user            : 'root',
-  password        : 'xZtD4Egw',
+  password        :  process.env.PW,
   database        : 'mydb'
 });
 
